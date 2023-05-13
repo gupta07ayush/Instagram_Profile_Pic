bcb81634bb3b6bae40c9d8d0e1658b619943ec46
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-13 23:23:47 +0530

    Enter User name Label

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ heading = Label(root, text='Instagram Profile Pic\n Downloader',
                 bg="#F56040", font=('times new roman', 40, 'bold'))
 heading.place(x=20, y=20, width=660, height=150)
 
+# Enter Username Label
+user_label = Label(root, text="Enter User Name:",
+                   bg="#FCAF45", fg='black', font=('Helvetika', 14, 'bold'))
+user_label.place(x=20, y=200, width=200, height=40)
+
 
 # Start the GUI
 root.mainloop()
