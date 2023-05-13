2ce6b147804c2db294144ec2d980b3cdc2fe7474
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-13 23:07:32 +0530

    Set geometry, title and bg color

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Button
 
 # Create an object of Tk Class
 root = Tk()
+
+# Set the title of the root Window
+root.title("Instagram Profile Pic Downloader")
+
+# Set the window size
+root.geometry('700x400')
+
+# Set the backgroud color of the root window
+root.config(bg="#405DE6")
 
 
 # Start the GUI
