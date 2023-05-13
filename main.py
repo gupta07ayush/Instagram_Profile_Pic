511c1ca1b9e1fcb60f5ccf017c87e1e627454fe7
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-13 23:12:07 +0530

    Heading Label

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ root.geometry('700x400')
 # Set the backgroud color of the root window
 root.config(bg="#405DE6")
 
+# Label for the heading
+heading = Label(root, text='Instagram Profile Pic\n Downloader',
+                bg="#F56040", font=('times new roman', 40, 'bold'))
+heading.place(x=20, y=20, width=660, height=150)
+
 
 # Start the GUI
 root.mainloop()
